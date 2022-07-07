ed285fc5f681c480a0e6097794c726d1eda02773
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-07-07 15:25:12 +0900

    updated main.c to be short

--- srcs/input/temp.c
+++ srcs/input/temp.c
@@ -1,20 +1,35 @@
+//#include"../../libft/libft.h" //need to modify the path
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/*
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+*/
 int	main(void)
 {
 	char *line;
 
 	line = NULL;
-	while (1)
+	while ((line = readline("minishell> ")))
 	{
-		line = readline("minishell> ");
-		if (line == NULL || ft_strlen(line) == 0)
-		{
-			free(line);
-			break;
-		}
-//		printf("line is '%s'\n", line);
 		add_history(line);
 		free(line);
 	}
+	free(line);
 	printf("exit\n");
+//	rl_clear_history;  ->I guess it's not necessary.
 	return (0);
 }
