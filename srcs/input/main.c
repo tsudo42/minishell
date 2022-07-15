73d0d6d761e41450dd85ea8cab84751054129ced
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-15 21:02:47 +0900

    tweek signal handler again

--- srcs/input/main.c
+++ srcs/input/main.c
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	while (s[len] != '\0')
 		len++;
 	return (len);
@@ -26,14 +28,19 @@ bool	ft_is_continue(char	*line)
 {
 	if (ft_strlen(line) >= ARG_MAX_SIZE)
 	{
-		(void)line;
+		printf("line too long\n");
+		return (true);
 	}
-	return (true);
+	return (false);
 }
 
 void	ft_signal_handler(int sig)
 {
 	(void)sig;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 	g_status = STATUS_FAILURE;
 }
 
@@ -42,28 +49,22 @@ int	main(void)
 	char *line;
 
 	line = NULL;
-//	printf("%d\n", getpid());
-	signal(SIGINT, ft_signal_handler);
-	signal(SIGQUIT, SIG_IGN);
+	//	printf("%d\n", getpid());
 	while (1)
 	{
+		signal(SIGINT, ft_signal_handler);
+		signal(SIGQUIT, SIG_IGN);
 		g_status = 0;
 		line = readline("minishell> ");
-		if (g_status == STATUS_FAILURE)
-		{
-			write(1, "\n", 1);
-			rl_replace_line("", 0);
-			rl_on_new_line();
-			rl_redisplay();
-		}
+//		if (g_status == STATUS_FAILURE)
 		if (ft_is_continue(line))
 			continue;
 		if (line == NULL)
 			break ;
 		add_history(line);
-		//		free(line);
+		free(line); // has to be deleted when the compilation with lexer.
 	}
-	//	free(line);
+	free(line); // has to be deleted when the compilation with lexer.
 	printf("exit\n");
 	//	rl_clear_history;  ->I guess it's not necessary.
 	return (g_status);
