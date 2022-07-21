89e51ffee34867583cdcc3788b35ce68f1009621
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-22 07:55:41 +0900

    1st commit

---
+++ srcs/expander/.dev/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define READ  (0)
+#define WRITE (1)
+
+#define STATUS_FAILURE -1
+#define ARG_MAX_SIZE 4096
+
+int	g_status;
+
+void	ft_exec_heredoc(char *delimi);
+
+//size_t	ft_strlen(const char *s);
+
+int ft_heredoc(char *delimi);
+
+#endif
