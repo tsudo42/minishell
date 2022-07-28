009fbc0b48eb6f089f677cafa6cb59308c38fc41
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 15:37:14 +0900

    1st draft. already tested by the tester

--- srcs/builtin/builtin_pwd.c
+++ srcs/builtin/builtin_pwd.c
@@ -14,6 +14,34 @@
 
 int	builtin_pwd(char **argv)
 {
+	char	*path;
+
 	(void)argv;
+	if ((path = getcwd(NULL, 0)) == NULL)
+		return (STATUS_FAILURE);
+	ft_putendl_fd(path, STDOUT_FILENO);
+	free(path);
+	return (STATUS_SUCCESS);
+}
+
+/*teter
+int main(void)
+{
+	char *argv[10];
+	char str[10] = "echo";
+	//	char str1[10] = "-n";
+	char str1[10] = "str1";
+	char str2[10] = "str2";
+	char str3[10] = "str3";
+	char *str4;
+
+	str4 = NULL;
+	argv[0] = str;
+	argv[1] = str1;
+	argv[2] = str2;
+	argv[3] = str3;
+	argv[4] = str4;
+	builtin_pwd(argv);
 	return (0);
 }
+*/
