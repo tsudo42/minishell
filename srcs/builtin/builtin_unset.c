b8cdebce0c88ffa0fec969e867fa0e3bdc1e42e3
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 22:16:46 +0900

    1st draft

--- srcs/builtin/builtin_unset.c
+++ srcs/builtin/builtin_unset.c
@@ -14,6 +14,30 @@
 
 int	builtin_unset(char **argv)
 {
-	(void)argv;
+	int	i;
+
+	i = 1;
+	while (argv[i] != NULL)
+		ft_unsetenv(argv[i++]);
+	return (STATUS_SUCCESS);
+}
+
+int main(void)
+{
+	char *argv[10];
+	char str[10] = "echo";
+	//	char str1[10] = "-n";
+	char str1[10] = "str1";
+	char str2[10] = "str2";
+	char str3[10] = "str3";
+	char *str4;
+
+	str4 = NULL;
+	argv[0] = str;
+	argv[1] = str1;
+	argv[2] = str2;
+	argv[3] = str3;
+	argv[4] = str4;
+	builtin_unset(argv);
 	return (0);
 }
