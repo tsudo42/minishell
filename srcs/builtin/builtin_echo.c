3db101e6d3406506e0c7ff9602f33dbefe348494
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 13:04:08 +0900

    1st draft

--- srcs/builtin/builtin_echo.c
+++ srcs/builtin/builtin_echo.c
@@ -14,6 +14,38 @@
 
 int	builtin_echo(char **argv)
 {
-	(void)argv;
+	bool	print_newline;
+
+	print_newline = true;
+	argv++;
+	if (*argv != NULL && ft_strcmp("-n", *argv) == 0)
+	{
+		argv++;
+		print_newline = false;
+	}
+	while (*argv != NULL)
+	{
+		ft_putstr_fd(*argv, STDOUT_FILENO);
+		argv++;
+		if (*argv != NULL)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+	}
+	if (print_newline == true)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (STATUS_SUCCESS);
+}
+
+int main(void)
+{
+	char *argv[10];
+	char str1[10] = "str1";
+	char str2[10] = "str2";
+	char str3[10] = "str3";
+	char str4[10] = "\0";
+	argv[0] = str1;
+	argv[1] = str2;
+	argv[2] = str3;
+	argv[3] = str4;
+	builtin_echo(argv);
 	return (0);
 }
