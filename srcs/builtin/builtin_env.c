ab622c493802cf34d3bba84c4570700d0652f6d2
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 22:14:20 +0900

    1st draft is done, but need to check tester once the environ function is created

--- srcs/builtin/builtin_env.c
+++ srcs/builtin/builtin_env.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_exit.c                                     :+:      :+:    :+:   */
+/*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,8 +12,41 @@
 
 #include "builtin.h"
 
-int	builtin_exit(char **argv)
+int	builtin_env(char **argv)
 {
+	t_env		**env;
+	t_env		*tmp_env;
+
 	(void)argv;
+	env = ft_getenv();
+	tmp_env = *env;
+	while (tmp_env != NULL)
+	{
+		ft_putstr_fd(tmp_env->env_var, STDOUT_FILENO);
+		ft_putstr_fd("=", STDOUT_FILENO);
+		ft_putstr_fd(tmp_env->content, STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		tmp_env = tmp_env->next;
+	}
+	return (STATUS_SUCCESS);
+}
+
+int main(void)
+{
+	char *argv[10];
+	char str[10] = "echo";
+	//	char str1[10] = "-n";
+	char str1[10] = "str1";
+	char str2[10] = "str2";
+	char str3[10] = "str3";
+	char *str4;
+
+	str4 = NULL;
+	argv[0] = str;
+	argv[1] = str1;
+	argv[2] = str2;
+	argv[3] = str3;
+	argv[4] = str4;
+	builtin_env(argv);
 	return (0);
 }
