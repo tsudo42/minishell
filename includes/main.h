9eb06237e03728a4dfe2c1f219052ab685d3c779
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 15:37:56 +0900

    1st commit: this header is for main.c in the srcs/input

---
+++ includes/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdbool.h>
+
+# define STATUS_FAILURE 0
+# define STATUS_SUCCESS 1
+# define ARG_MAX_SIZE 4096
+
+int	g_status;
+
+#endif
