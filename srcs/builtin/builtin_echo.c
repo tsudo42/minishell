fbf345874ee4f200e021ce2d59b96a6983359df0
hosuzuki <hokuto.suzuki@gmail.com>
2022-07-28 13:37:55 +0900

    2nd draft: the tester was added

--- srcs/builtin/builtin_echo.c
+++ srcs/builtin/builtin_echo.c
@@ -18,7 +18,7 @@ int	builtin_echo(char **argv)
 
 	print_newline = true;
 	argv++;
-	if (*argv != NULL && ft_strcmp("-n", *argv) == 0)
+	if (*argv != NULL && ft_strncmp("-n", *argv, 3) == 0)
 	{
 		argv++;
 		print_newline = false;
@@ -35,17 +35,24 @@ int	builtin_echo(char **argv)
 	return (STATUS_SUCCESS);
 }
 
+/* tester
 int main(void)
 {
 	char *argv[10];
+	char str[10] = "echo";
+//	char str1[10] = "-n";
 	char str1[10] = "str1";
 	char str2[10] = "str2";
 	char str3[10] = "str3";
-	char str4[10] = "\0";
-	argv[0] = str1;
-	argv[1] = str2;
-	argv[2] = str3;
-	argv[3] = str4;
+	char *str4;
+	
+	str4 = NULL;
+	argv[0] = str;
+	argv[1] = str1;
+	argv[2] = str2;
+	argv[3] = str3;
+	argv[4] = str4;
 	builtin_echo(argv);
 	return (0);
 }
+*/
