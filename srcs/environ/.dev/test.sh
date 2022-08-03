1406ff4f0c4f9498e7ffd6c5f36f3f9b574b618f
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-03 10:05:17 +0900

    1st commit of .dev depository

---
+++ srcs/environ/.dev/test.sh
@@ -0,0 +1,2 @@
+gcc -Wall -Werror -Wextra ../ft_getenv.c ../ft_init_environ.c ../ft_unsetenv.c ../ft_putenv.c ../ft_setenv.c ../../../libft/libft.a -I../../../includes -g
+#-I../../../libft/includes
