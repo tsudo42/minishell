6e22d6fb1eaa1de43ca19194ac2ca51606f5b741
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-07 14:57:21 +0900

    mod

--- srcs/builtin/.dev/test.sh
+++ srcs/builtin/.dev/test.sh
@@ -1,1 +1,3 @@
-gcc -Wall -Wextra -Werror -g ../builtin_exit.c ../../../libft/libft.a -I ../../../includes/ -I../../../libft/includes/
+gcc -Wall -Wextra -Werror -g ../builtin_unset.c ../builtin_env.c ../builtin_export.c ../../environ/ft_getenv.c ../../environ/ft_setenv.c ../../environ/ft_init_environ.c ../../environ/ft_unsetenv.c ../../environ/ft_putenv.c ../../../libft/libft.a -I ../../../includes/ -I../../../libft/includes/
+
+#gcc -Wall -Wextra -Werror -g ../builtin_echo.c ../../environ/ft_getenv.c ../../environ/ft_setenv.c ../../environ/ft_init_environ.c ../../environ/ft_unsetenv.c ../../environ/ft_putenv.c ../../../libft/libft.a -I ../../../includes/ -I../../../libft/includes/
