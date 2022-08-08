74643f60b76f32803a70c4583dbefbbdf2e8758e
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-08 20:31:59 +0900

    adding g_sig and exit_status func

--- srcs/minishell/main.c
+++ srcs/minishell/main.c
@@ -12,8 +12,17 @@
 
 #include "minishell.h"
 
+int *exit_status(void)
+{
+	static int s;
+
+	return (&s);
+}
+
 static bool	is_continue(char	*line)
 {
+	if (!line)
+		return (false);
 	if (ft_strlen(line) >= ARG_MAX_SIZE)
 	{
 		printf("line too long\n");
@@ -24,12 +33,12 @@ static bool	is_continue(char	*line)
 
 void	signal_handler(int sig)
 {
-	(void)sig;
+	g_sig = sig;
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
-	g_status = STATUS_FAILURE;
+//	g_status = STATUS_FAILURE;
 }
 
 int	main(void)
@@ -40,14 +49,17 @@ int	main(void)
 	line = NULL;
 	while (1)
 	{
+		g_sig = 0;
 		signal(SIGINT, signal_handler);
 		signal(SIGQUIT, SIG_IGN);
-		g_status = 0;
 		line = readline("minishell> ");
-		if (is_continue(line))
-			continue;
 		if (line == NULL)
 			break ;
+		if (g_sig != 0 || is_continue(line))
+		{
+			free (line);
+			continue ;
+		}
 		add_history(line);
 		ret = executor(parser(lexer(line)));
 	}
