fd3a544a8212949ca40f26c5e71c724f7fcdbbe5
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-08-11 18:29:48 +0900

    Fix bug in calculation of fd in exec_d

--- srcs/executor/exec_d.c
+++ srcs/executor/exec_d.c
@@ -45,20 +45,38 @@ static int	exec_d_heredoc(const char *word, int fd)
 	return (0);
 }
 
+static long	calc_fd(t_ast_d_type type, char *num, int *is_err)
+{
+	long	fd;
+
+	if (num == NULL || *num == '\0')
+	{
+		if (type == AST_D_REDIN)
+			return (0);
+		return (1);
+	}
+	fd = ft_strtol(num, NULL, 10);
+	if (fd < 0 || INT_MAX < fd)
+	{
+		errno = EBADF;
+		*is_err = 1;
+		perror("minishell: file descriptor out of range");
+		return (-1);
+	}
+	return (fd);
+}
+
 int	exec_d(t_ast_d *d)
 {
 	int		is_err;
 	long	fd;
 
 	is_err = 0;
-	while (d != NULL && is_err)
+	while (d != NULL && is_err == 0)
 	{
-		fd = ft_strtol(d->num, NULL, 10);
-		if (fd > 3)
-		{
-			perror("minishell: file descriptor out of range");
+		fd = calc_fd(d->type, d->num, &is_err);
+		if (fd < 0)
 			return (1);
-		}
 		if (d->type == AST_D_REDIN)
 			is_err = exec_d_redin(d->word, fd);
 		else if (d->type == AST_D_REDOUT)
