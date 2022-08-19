20c4c908581e656d33cfc2b221d392b4a86db1ad
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-19 19:46:03 +0900

    delete comment outs

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -28,7 +28,6 @@ int get_exit_status(char *str)
 		exit_status = exit_status % 256; 
 	else if (exit_status < 0)
 		exit_status = 0;
-//		exit_status = exit_status 
 return (exit_status);//change this
 }
 
@@ -37,11 +36,11 @@ int	builtin_exit(char **argv)
 	int	exit_status;
 
 	if (argv[1] == NULL)
-		exit (0);
+		exit (0); // check this
 	if (argv[2] != NULL)
 	{
 		ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
-		return (STATUS_FAILURE);// has to reaturn 1
+		return (STATUS_FAILURE);// has to return 1
 	}
 	if ((exit_status = get_exit_status(argv[1])) == -1)
 	{
@@ -50,25 +49,3 @@ int	builtin_exit(char **argv)
 	}
 	exit(exit_status); //change this
 }
-
-/*tester
-int main(void)
-{
-	char *argv[10];
-	char str[10] = "echo";
-	//	char str1[10] = "-n";
-	char str1[10] = "str1";
-	char str2[10] = "str2";
-	char str3[10] = "str3";
-	char *str4;
-
-	str4 = NULL;
-	argv[0] = str;
-	argv[1] = str1;
-	argv[2] = str2;
-	argv[3] = str3;
-	argv[4] = str4;
-	builtin_exit(argv);
-	return (0);
-}
-*/
