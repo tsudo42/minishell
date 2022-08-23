e2f4a99198aceeee10e47580158432d7ab5595f6
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-23 12:06:52 +0900

    1st commit

---
+++ includes/minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
+/*   Updated: 2022/07/01 00:00:00 by tsudo            ###   ##########        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdbool.h>
+# include "libft.h"
+# include "parser.h"
+# include "lexer.h"
+# include "exec.h"
+
+# define STATUS_FAILURE -1
+# define STATUS_SUCCESS 1
+# define ARG_MAX_SIZE 4096
+
+volatile sig_atomic_t	g_sig;
+
+void activate_signal(void);
+void deactivate_signal(void);
+void activate_signal_heredoc(void);
+int *exit_status(void);
+
+#endif /* MINISHELL_H */
