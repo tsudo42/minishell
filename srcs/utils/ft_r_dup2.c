c2817717590cb9e26d62dc28093402c57e09da3c
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-24 11:49:05 +0900

    fixed typo

--- srcs/utils/ft_r_dup2.c
+++ srcs/utils/ft_r_dup2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_x_dup2.c                                        :+:      :+:    :+:   */
+/*   ft_r_dup2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/07/01 00:00:00 by tsudo            ###   ##########        */
+/*   Updated: 2022/08/24 11:48:55 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 
 /* ************************************************************************** */
 /*  This function is an error checking version of dup2(2).                    */
-/*  This function prints an error message and terminates the process calling  */
-/*  exit(3) when dup2 faces error.                                            */
+/*  This function prints an error message and return -1 when dup2 faces       */
+/*  error.                                                                    */
 /* ************************************************************************** */
 int	ft_r_dup2(int fildes, int fildes2, const char *errmsg)
 {
