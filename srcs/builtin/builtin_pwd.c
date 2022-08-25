8539d4bd96ccc3e0730c5df9cc4113ab3bf7f191
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-25 14:53:40 +0900

    adding perror

--- srcs/builtin/builtin_pwd.c
+++ srcs/builtin/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/07/01 00:00:00 by tsudo            ###   ##########        */
+/*   Updated: 2022/08/25 14:24:16 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ int	builtin_pwd(char **argv)
 
 	(void)argv;
 	if ((path = getcwd(NULL, 0)) == NULL)
+	{
+		perror(BUILT_ERRMSG ": getcwd");
 		return (STATUS_FAILURE);
+	}
 	ft_putendl_fd(path, STDOUT_FILENO);
 	free(path);
 	return (STATUS_SUCCESS);
