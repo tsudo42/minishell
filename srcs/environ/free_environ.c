f7ec6402f32b60657b2df60fdb9fb42574f0cdb0
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-25 19:54:30 +0900

    fixed.

--- srcs/environ/free_environ.c
+++ srcs/environ/free_environ.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/07/01 00:00:00 by tsudo            ###   ##########        */
+/*   Updated: 2022/08/25 19:53:51 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ void free_environ(void)
 	extern char **environ;
 	int i;
 
-	if (!environ || !(*environ))
+	if (!environ)
 		return ;
 	i = 0;
 	while (environ[i] != NULL)
 		free (environ[i++]);
-	free (environ[i]);
 	free (environ);
 	environ = NULL;
 	return ;
