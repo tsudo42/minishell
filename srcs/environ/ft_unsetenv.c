bba33fc0fa5fd0fe22a813b49f54f7b45cc26510
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-25 14:54:39 +0900

    adding free_env_new

--- srcs/environ/ft_unsetenv.c
+++ srcs/environ/ft_unsetenv.c
@@ -6,11 +6,24 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/07/01 00:00:00 by tsudo            ###   ##########        */
+/*   Updated: 2022/08/25 13:06:49 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environ.h"
+
+static void free_env_new(char **env_new, int i)
+{
+	int j;
+
+	j = 0;
+	if (!env_new)
+		return ;
+	while(j < i - 2)
+		free(env_new[j++]);
+	free (env_new);
+	return ;
+}
 
 static int envdup_unset(char **env_new, int skip)
 {
@@ -28,7 +41,10 @@ static int envdup_unset(char **env_new, int skip)
 		{
 			env_new[i] = ft_strdup(environ[j++]);
 			if (!env_new[i++])
+			{
+				free_env_new(env_new, i);
 				return (-1);
+			}
 		}
 	}
 	env_new[i] = NULL;
@@ -63,7 +79,7 @@ int	ft_unsetenv(const char *name)
 	extern char **environ;
 	int skip;
 
-	init_environ();
+//	init_environ();
 	if ((skip = find_name(name)) == -1)
 		return (0);
 	environ = del_one_environ(skip);
