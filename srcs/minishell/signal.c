e6e559f1dc0ae5cc9b35b6abd042f0685bf0febd
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-26 09:06:06 +0900

    reduced the contents inside signal_handler

--- srcs/minishell/signal.c
+++ srcs/minishell/signal.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/25 12:21:31 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/26 08:54:38 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ static void	signal_handler(int sig)
 {
 	g_sig = sig;
 	write(1, "\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
+//	rl_replace_line("", 0);
+//	rl_on_new_line();
+//	rl_redisplay();
 }
 
-/*
 int rl_status_checker(void)
 {
 	if (g_sig != 0)
 	{
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-//		rl_event_hook = 0;
+//		rl_replace_line("", 0);
+//		rl_on_new_line();
+//		rl_redisplay();
+		rl_event_hook = 0;
 		rl_done = 1;
 	}
 	return (0);
 }
-*/
+
 void activate_signal(void)
 {
 	signal(SIGINT, signal_handler);
