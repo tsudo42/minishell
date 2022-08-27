57f864d71eb3f2f2fcbf1e514042ee57ec2ee1da
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-27 11:39:40 +0900

    removed comments out

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/26 18:06:05 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/27 11:39:06 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	builtin_get_exit_status(char *str)
 		exit_status = exit_status % 256;
 	else if (exit_status < 0)
 		exit_status = 0;
-	return (exit_status);//change this
+	return (exit_status);
 }
 
 int	builtin_exit(char **argv)
@@ -36,7 +36,7 @@ int	builtin_exit(char **argv)
 	int	exit_status;
 
 	if (argv[1] == NULL)
-		exit (0); // check this
+		exit (0);
 	if (argv[2] != NULL)
 	{
 		ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
@@ -45,8 +45,8 @@ int	builtin_exit(char **argv)
 	exit_status = builtin_get_exit_status(argv[1]);
 	if (exit_status == -1)
 	{
-		ft_putstr_fd("exit: numeric argument required\n", STDERR_FILENO);
+		ft_putstr_fd("exit: a numeric argument required\n", STDERR_FILENO);
 		exit (255);
 	}
-	exit(exit_status); //change this
+	exit(exit_status);
 }
