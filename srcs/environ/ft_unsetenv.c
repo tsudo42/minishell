062a4ed4f74b3a8f2d3d35534a0043900bef0abe
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-29 13:14:42 +0900

    adding find_name function and ft_x function

--- srcs/environ/ft_unsetenv.c
+++ srcs/environ/ft_unsetenv.c
@@ -6,13 +6,37 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:17:37 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/29 13:13:21 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environ_internal.h"
 
-static int	envdup_unset(char **new_env, int skip)
+static int	find_name(const char *name)
+{
+	extern char	**environ;
+	int			i;
+	int			len;
+	int			len_env;
+
+	i = 0;
+	len = ft_strlen(name);
+	while (environ[i])
+	{
+		len_env = ft_strlen(environ[i]);
+		if (len_env < len)
+		{
+			i++;
+			continue ;
+		}
+		if (ft_strncmp(environ[i], name, len) == 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static int	envdup_unset(char **new_env, int location)
 {
 	extern char	**environ;
 	int			i;
@@ -22,54 +46,39 @@ static int	envdup_unset(char **new_env, int skip)
 	j = 0;
 	while (environ[j])
 	{
-		if (j == skip)
+		if (j == location)
 			j++;
 		if (environ[j] != NULL)
-		{
-			new_env[i] = ft_strdup(environ[j++]);
-			if (!new_env[i++])
-			{
-				free_environ();
-				return (-1);
-			}
-		}
+			new_env[i++] = ft_x_strdup(environ[j++]);
 	}
 	new_env[i] = NULL;
 	return (0);
 }
 
-static char	**del_one_environ(int skip)
+static char	**del_one_environ(int location)
 {
 	extern char	**environ;
 	char		**new_env;
 
 	if (!environ)
 		return (NULL);
-	new_env = (char **)malloc(sizeof(char *) * (envlen(environ)));
-	if (!new_env)
-	{
-		free_environ();
-		return (NULL);
-	}
-	if (envdup_unset(new_env, skip) == -1)
-	{
-		free_environ();
-		return (NULL);
-	}
+	new_env = (char **)ft_x_malloc(sizeof(char *) * (envlen(environ)), \
+		ENVIRON_ERRMSG ": malloc");
+	envdup_unset(new_env, location);
 	free_environ();
-	environ = NULL;
 	return (new_env);
 }
 
 int	ft_unsetenv(const char *name)
 {
 	extern char	**environ;
-	int			skip;
+	int			location;
 
-	skip = find_name(name);
-	if (skip == -1)
+	location = find_name(name);
+	if (location == -1)
 		return (0);
-	environ = del_one_environ(skip);
+	environ = del_one_environ(location);
+	is_init_environ(1);
 	if (!environ)
 		ft_perror_exit(EXIT_FAILURE, ENVIRON_ERRMSG ": malloc");
 	return (0);
