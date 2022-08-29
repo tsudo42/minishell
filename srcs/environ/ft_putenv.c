f8da2ee9367e9f46ac4b5d026a82fcb24bcd8e33
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-29 13:12:32 +0900

    updated by using ft_x_strdup

--- srcs/environ/ft_putenv.c
+++ srcs/environ/ft_putenv.c
@@ -6,11 +6,33 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:17:37 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/29 13:11:56 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environ_internal.h"
+
+char	**add_environ(const char *string)
+{
+	extern char	**environ;
+	char		**new_env;
+	int			i;
+
+	if (!environ)
+		ft_perror_exit(EXIT_FAILURE, ENVIRON_ERRMSG ": environ");
+	new_env = (char **)ft_x_malloc(sizeof(char *) * (envlen(environ) + 2), \
+		ENVIRON_ERRMSG ": malloc");
+	i = 0;
+	while (environ[i])
+	{
+		new_env[i] = ft_x_strdup(environ[i]);
+		i++;
+	}
+	new_env[i++] = ft_x_strdup(string);
+	new_env[i] = NULL;
+	free_environ();
+	return (new_env);
+}
 
 static int	find_string(const char *string)
 {
@@ -35,9 +57,7 @@ static int	change_content(const char *string, int location)
 
 	free (environ[location]);
 	environ[location] = NULL;
-	environ[location] = ft_strdup(string);
-	if (environ[location] == NULL)
-		ft_perror_exit(EXIT_FAILURE, ENVIRON_ERRMSG ": malloc");
+	environ[location] = ft_x_strdup(string);
 	return (0);
 }
 
@@ -50,10 +70,6 @@ int	ft_putenv(const char *string)
 	if (location != -1)
 		return (change_content(string, location));
 	environ = add_environ(string);
-	if (!environ)
-	{
-		free_environ();
-		ft_perror_exit(EXIT_FAILURE, ENVIRON_ERRMSG ": malloc");
-	}
+	is_init_environ(1);
 	return (0);
 }
