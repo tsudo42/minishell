edf4d9c4fb0bbedd2207094c697a2459199199a7
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-29 13:11:30 +0900

    added is_init_environ

--- srcs/environ/free_environ.c
+++ srcs/environ/free_environ.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:17:37 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/29 13:11:03 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ void	free_environ(void)
 
 	if (!environ)
 		return ;
+	if (is_init_environ(0) != ENV_INITIALIZED)
+		return ;
+	printf("free_environ(before): %p\n\n", environ);
 	i = 0;
 	while (environ[i] != NULL)
 		free (environ[i++]);
+	printf("free_environ(after): %p\n\n", environ);
 	free (environ);
 	environ = NULL;
 	return ;
