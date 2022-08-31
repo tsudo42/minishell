9e5f7ad30c95d36ff4bbb3d992f6f70c28defa93
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-31 15:59:36 +0900

    updated the ft_r_dup2's discription.

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 08:42:47 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/31 15:16:49 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ int		ft_x_dup2(int fildes, int fildes2, const char *errmsg);
 
 /* ************************************************************************** */
 /*  This function is an error checking version of dup2(2).                    */
-/*  This function prints an error message and terminates the process calling  */
-/*  return when dup2 faces error.                                             */
+/*  This function prints an error message and terminates the process and      */
+/*  return the new file discriptor. When dup2 faces error, -1 is returned.    */
 /* ************************************************************************** */
 int		ft_r_dup2(int fildes, int fildes2, const char *errmsg);
 
@@ -70,13 +70,6 @@ void	*ft_x_malloc(size_t size, const char *errmsg);
 /*  exit(3) when pipe faces error.                                           */
 /* ************************************************************************** */
 void	ft_x_pipe(int fildes[2], const char *errmsg);
-
-/* ************************************************************************** */
-/*  This function is an version of ft_putendl_fd to return the length of      */
-/*  charactors printed.                                 ..                    */
-/* ************************************************************************** */
-
-//int	ft_putendl_len_fd(const char *s, int fd);
 
 void	ft_perror_exit(int exit_no, const char *errmsg);
 char	*ft_x_strdup(const char *s);
