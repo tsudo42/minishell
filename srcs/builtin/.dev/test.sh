88d2782159fbcebbdbb137c1dab38b167121c032
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-31 16:00:38 +0900

    deleted .dev directories

--- srcs/builtin/.dev/test.sh
+++
@@ -1,3 +0,0 @@
-gcc -Wall -Wextra -Werror -g ../builtin_unset.c ../builtin_env.c ../builtin_export.c ../../environ/ft_getenv.c ../../environ/ft_setenv.c ../../environ/ft_init_environ.c ../../environ/ft_unsetenv.c ../../environ/ft_putenv.c ../../../libft/libft.a -I ../../../includes/ -I../../../libft/includes/
-
-#gcc -Wall -Wextra -Werror -g ../builtin_echo.c ../../environ/ft_getenv.c ../../environ/ft_setenv.c ../../environ/ft_init_environ.c ../../environ/ft_unsetenv.c ../../environ/ft_putenv.c ../../../libft/libft.a -I ../../../includes/ -I../../../libft/includes/
--- srcs/environ/.dev/test.sh
+++
@@ -1,2 +0,0 @@
-gcc -Wall -Werror -Wextra ../ft_getenv.c ../ft_init_environ.c ../ft_unsetenv.c ../ft_putenv.c ../ft_setenv.c ../../../libft/libft.a -I../../../includes -g
-#-I../../../libft/includes
