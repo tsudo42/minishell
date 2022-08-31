ec19817ac418bcba857815f351769787e9214de7
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-31 16:03:42 +0900

    change perror to ft_putendl_fd when home_dir == NULL

--- srcs/builtin/builtin_cd.c
+++ srcs/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:15:34 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/31 15:50:01 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ static int	cd_to_home(void)
 	home_dir = ft_getenv("HOME");
 	if (home_dir == NULL)
 	{
-		perror(BUILT_ERRMSG": home_dir");
+		ft_putendl_fd("cd: No Home", STDERR_FILENO);
 		return (STATUS_FAILURE);
 	}
 	if (chdir(home_dir) == -1)
 	{
-		perror(BUILT_ERRMSG": chdir");
+		perror(BUILT_ERRMSG ": chdir");
 		return (STATUS_FAILURE);
 	}
 	return (STATUS_SUCCESS);
@@ -36,7 +36,7 @@ int	builtin_cd(char **argv)
 		return (cd_to_home());
 	if (chdir(argv[1]) == -1)
 	{
-		perror("builtin_cd");
+		perror(BUILT_ERRMSG ": chdir");
 		return (STATUS_FAILURE);
 	}
 	return (STATUS_SUCCESS);
