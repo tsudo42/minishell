79702cd1c3b4292ab1d9da004d875ef6a74ad057
hosuzuki <hokuto.suzuki@gmail.com>
2022-08-31 16:07:53 +0900

    added a conditon if environ is NULL, return -1

--- srcs/builtin/builtin_env.c
+++ srcs/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/27 17:15:34 by hos              ###   ########.fr       */
+/*   Updated: 2022/08/31 15:00:13 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,12 @@ int	builtin_env(char **argv)
 	extern char	**environ;
 	int			i;
 
+	if (!environ)
+		return (STATUS_FAILURE);
 	(void)argv;
 	i = 0;
 	while (environ[i])
 	{
-		if (ft_strncmp(environ[i], "LINES=", 6) == 0)
-		{
-			i++;
-			continue ;
-		}
-		else if (ft_strncmp(environ[i], "COLUMNS=", 8) == 0)
-		{
-			i++;
-			continue ;
-		}
 		ft_putstr_fd(environ[i++], STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	}
