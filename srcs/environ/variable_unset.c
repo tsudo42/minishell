73dfd07be6d14f183f488a660ca43a4ff46bc1aa
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-08 22:15:54 +0900

    added a function to to check if the var is PWD to keep PWD with that it's is_exported is 0.

--- srcs/environ/variable_unset.c
+++ srcs/environ/variable_unset.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   variable_unset.c                                  :+:      :+:    :+:   */
+/*   variable_unset.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/31 15:09:46 by hosuzuki         ###   ########.fr       */
+/*   Updated: 2022/09/08 21:46:36 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,23 @@ static t_var	*drop_var(t_var *var)
 	return (next);
 }
 
+static int	is_pwd(t_var *var, t_environ *env)
+{
+	if (ft_strcmp(var->key, "PWD") == 0)
+	{
+		variable_set("PWD", variable_get("PWD", env), 0, env);
+		return (1);
+	}
+	return (0);
+}
+
 /**
  *  This function gets string of variable selected by key if found.
  *  Otherwise, NULL is returned.
  *
  *  Argument key is checked via variable_check_key_format().
  */
+
 int	variable_unset(const char *key, t_environ *env)
 {
 	t_var	*var;
@@ -40,14 +51,16 @@ int	variable_unset(const char *key, t_environ *env)
 	var = env->vars;
 	if (var->key != NULL && ft_strcmp(var->key, key) == 0)
 	{
-		env->vars = drop_var(var);
+		if (!is_pwd(var, env))
+			env->vars = drop_var(var);
 		return (0);
 	}
 	while (var->next != NULL)
 	{
 		if (var->next->key != NULL && ft_strcmp(var->next->key, key) == 0)
 		{
-			var->next = drop_var(var->next);
+			if (!is_pwd(var->next, env))
+				var->next = drop_var(var->next);
 			return (0);
 		}
 		var = var->next;
