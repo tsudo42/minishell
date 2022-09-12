62e2a15fd6d29f602fab06ab75a254810278fd95
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-12 19:16:25 +0900

    experiment

--- srcs/builtin/builtin_cd.c
+++ srcs/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/09/12 08:30:41 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/12 16:52:35 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static int	cd_to_home(t_environ *env)
 {
 	char	*home_dir;
+	char	buf[PATH_MAX];
 
 	home_dir = variable_get("HOME", env);
 	if (home_dir == NULL)
@@ -22,13 +23,20 @@ static int	cd_to_home(t_environ *env)
 		ft_putendl_fd(CD_ERRMSG ": No Home", STDERR_FILENO);
 		return (STATUS_FAILURE);
 	}
+	if (getcwd(buf, sizeof(buf)) == NULL)
+	{
+		perror(CD_ERRMSG ": getcwd");
+		return (STATUS_FAILURE);
+	}
+	if (variable_get("OLDPWD"))
+		variable_set("OLDPWD", buf, 1, env);
 	if (chdir(home_dir) == -1)
 	{
 		perror(CD_ERRMSG ": chdir");
 		return (STATUS_FAILURE);
 	}
-	variable_set("OLDPWD", variable_get("PWD", env), 0, env);
-	variable_set("PWD", home_dir, 0, env);
+	if (variable_get("PWD"))
+		variable_set("PWD", buf, 0, env);
 	return (STATUS_SUCCESS);
 }
 
@@ -40,17 +48,24 @@ int	builtin_cd(char **argv, t_environ *env)
 		return (STATUS_FAILURE);
 	if (argv[1] == NULL)
 		return (cd_to_home(env));
-	if (chdir(argv[1]) == -1)
-	{
-		perror(CD_ERRMSG ": chdir");
-		return (STATUS_FAILURE);
-	}
-	variable_set("OLDPWD", variable_get("PWD", env), 0, env);
 	if (getcwd(buf, sizeof(buf)) == NULL)
 	{
 		perror(CD_ERRMSG ": getcwd");
 		return (STATUS_FAILURE);
 	}
-	variable_set("PWD", buf, 0, env);
+	if (variable_get("OLDPWD"))
+		variable_set("OLDPWD", buf, 1, env);
+	if (chdir(argv[1]) == -1)
+	{
+		perror(CD_ERRMSG ": chdir");
+		return (STATUS_FAILURE);
+	}
+	if (getcwd(buf, sizeof(buf)) == NULL)
+	{
+		perror(CD_ERRMSG ": getcwd");
+		return (STATUS_FAILURE);
+	}
+	if (variable_get("PWD"))
+		variable_set("PWD", buf, 0, env);
 	return (STATUS_SUCCESS);
 }
