c84637aa7fe03dc35f6e687ea0cabcb1bc1eae39
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-12 12:25:33 +0900

    change the condition in the builtin_exit to deal with the input such as "exit a 1 3 3". Also, long max handling is done.

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -6,13 +6,13 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/31 15:06:25 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/12 12:21:58 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-static int	builtin_get_exit_status(char *str)
+static long	builtin_get_exit_status(char *str)
 {
 	long	num;
 	char	*endptr;
@@ -24,24 +24,56 @@ static int	builtin_get_exit_status(char *str)
 	return (num);
 }
 
+static void	builtin_put_msg_and_exit(char *str, char *argv, int status)
+{
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (str)
+	{
+		ft_putstr_fd(EXIT_ERRMSG, STDERR_FILENO);
+		if (argv)
+		{
+			ft_putstr_fd(": ", STDERR_FILENO);
+			ft_putstr_fd(argv, STDERR_FILENO);
+		}
+		ft_putstr_fd(str, STDERR_FILENO);
+	}
+	exit (status);
+}
+
+static int	is_numeric(char *str)
+{
+	if (!str)
+		return (-1);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
 int	builtin_exit(char **argv, t_environ *env)
 {
-	int	exit_status;
+	long	exit_status;
 
+	if (argv == NULL || *argv == NULL)
+		return (STATUS_FAILURE);
 	(void)env;
 	if (argv[1] == NULL)
-		exit (0);
+		builtin_put_msg_and_exit(NULL, NULL, 0);
+	if (!is_numeric(argv[1]))
+		builtin_put_msg_and_exit(": numeric argument required\n", argv[1], 255);
 	if (argv[2] != NULL)
 	{
+		ft_putstr_fd("exit\n", STDERR_FILENO);
 		ft_putstr_fd(EXIT_ERRMSG ": too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
 	exit_status = builtin_get_exit_status(argv[1]);
 	if (exit_status == -1)
-	{
-		ft_putstr_fd(\
-			EXIT_ERRMSG ": a numeric argument required\n", STDERR_FILENO);
-		return (255);
-	}
-	exit(exit_status);
+		builtin_put_msg_and_exit(": numeric argument required\n", \
+			argv[1], 255);
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	exit (exit_status);
 }
