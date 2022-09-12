773dad1b0d177933425e768d70f2ebdbe7236da5
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-13 08:59:31 +0900

    to deal with `export OLDPWD` after unsettting OLDPWD and cd.

--- srcs/builtin/builtin_env.c
+++ srcs/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/08/31 15:00:13 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/13 08:57:04 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	builtin_env(char **argv, t_environ *env)
 	var = env->vars;
 	while (var != NULL)
 	{
-		if (var->key != NULL && var->value != NULL && var->is_exported)
+		if (var->key != NULL && var->value != NULL && var->is_exported \
+			&& var->value[0] != '\0')
 			ft_dprintf(STDOUT_FILENO, "%s=%s\n", var->key, var->value);
 		var = var->next;
 	}
