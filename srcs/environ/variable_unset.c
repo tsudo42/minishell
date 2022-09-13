0e63fb088308a33d8b822202cd67548b710f2d06
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-13 11:12:19 +0900

    removed is_pwd function

--- srcs/environ/variable_unset.c
+++ srcs/environ/variable_unset.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/09/09 13:07:16 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/12 21:48:58 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,12 @@ static t_var	*drop_var(t_var *var)
 	return (next);
 }
 
-static int	is_pwd(t_var *var, t_environ *env)
-{
-	if (ft_strcmp(var->key, "PWD") == 0)
-	{
-		variable_set("PWD", variable_get("PWD", env), -1, env);
-		return (1);
-	}
-	return (0);
-}
-
 /**
  *  This function gets string of variable selected by key if found.
  *  Otherwise, NULL is returned.
  *
  *  Argument key is checked via variable_check_key_format().
  */
-
 int	variable_unset(const char *key, t_environ *env)
 {
 	t_var	*var;
@@ -51,16 +40,14 @@ int	variable_unset(const char *key, t_environ *env)
 	var = env->vars;
 	if (var->key != NULL && ft_strcmp(var->key, key) == 0)
 	{
-		if (!is_pwd(var, env))
-			env->vars = drop_var(var);
+		env->vars = drop_var(var);
 		return (0);
 	}
 	while (var->next != NULL)
 	{
 		if (var->next->key != NULL && ft_strcmp(var->next->key, key) == 0)
 		{
-			if (!is_pwd(var->next, env))
-				var->next = drop_var(var->next);
+			var->next = drop_var(var->next);
 			return (0);
 		}
 		var = var->next;
