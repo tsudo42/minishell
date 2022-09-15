5cb6d990267b406a5918390efcfd9c13897a65fa
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-09-15 16:10:19 +0900

    Fix: allow trailing spaces of numbers

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -19,7 +19,9 @@ static long	builtin_get_exit_status(char *str)
 
 	errno = 0;
 	num = ft_strtol(str, &endptr, 10);
-	if (errno || *endptr != '\0')
+	while (ft_isspace(*endptr))
+		endptr++;
+	if (*endptr != '\0' || errno != 0)
 		return (-1);
 	return (num);
 }
@@ -39,8 +41,8 @@ int	builtin_exit(char **argv, t_environ *env)
 	exit_status = builtin_get_exit_status(argv[1]);
 	if (exit_status == -1)
 	{
-		ft_dprintf(STDERR_FILENO, "exit\n%s: %s: \
-			numeric argument required\n", EXIT_ERRMSG, argv[1]);
+		ft_dprintf(STDERR_FILENO, \
+			"exit\n%s: %s: numeric argument required\n", EXIT_ERRMSG, argv[1]);
 		exit (255);
 	}
 	if (argv[2] != NULL)
