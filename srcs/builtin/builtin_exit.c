00271aa0a017d3a3de4935e43c4d2a8b5788a667
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-15 13:37:42 +0900

    deleted builtin_put_msg_and_exit

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 00:00:00 by tsudo             #+#    #+#             */
-/*   Updated: 2022/09/12 15:31:16 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/15 12:29:52 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,6 @@ static long	builtin_get_exit_status(char *str)
 	return (num);
 }
 
-static void	builtin_put_msg_and_exit(char *str, char *argv, int status)
-{
-	ft_putstr_fd("exit\n", STDERR_FILENO);
-	if (str)
-	{
-		ft_putstr_fd(EXIT_ERRMSG, STDERR_FILENO);
-		if (argv)
-		{
-			ft_putstr_fd(": ", STDERR_FILENO);
-			ft_putstr_fd(argv, STDERR_FILENO);
-		}
-		ft_putstr_fd(str, STDERR_FILENO);
-	}
-	exit (status);
-}
-
 int	builtin_exit(char **argv, t_environ *env)
 {
 	long	exit_status;
@@ -48,15 +32,20 @@ int	builtin_exit(char **argv, t_environ *env)
 		return (STATUS_FAILURE);
 	(void)env;
 	if (argv[1] == NULL)
-		builtin_put_msg_and_exit(NULL, NULL, 0);
+	{
+		ft_dprintf(STDERR_FILENO, "exit\n");
+		exit (1);
+	}
 	exit_status = builtin_get_exit_status(argv[1]);
 	if (exit_status == -1)
-		builtin_put_msg_and_exit(": numeric argument required\n", \
-			argv[1], 255);
+	{
+		ft_dprintf(STDERR_FILENO, "exit\n%s: %s: \
+			numeric argument required\n", EXIT_ERRMSG, argv[1]);
+		exit (255);
+	}
 	if (argv[2] != NULL)
 	{
-		ft_putstr_fd("exit\n", STDERR_FILENO);
-		ft_putstr_fd(EXIT_ERRMSG ": too many arguments\n", STDERR_FILENO);
+		ft_dprintf(STDERR_FILENO, "exit\n%s: too many arguments\n", EXIT_ERRMSG);
 		return (1);
 	}
 	ft_putstr_fd("exit\n", STDERR_FILENO);
