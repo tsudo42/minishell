e4cc7c21e7f9ae0dd59174349a8ff912938c58d5
hosuzuki <hokuto.suzuki@gmail.com>
2022-09-17 19:14:17 +0900

    added a condition if ft_lstnew return an error and changed a condition for while loop to add the var at the last in the var's list.

--- srcs/builtin/builtin_export2.c
+++ srcs/builtin/builtin_export2.c
@@ -6,7 +6,7 @@
 /*   By: hos <hosuzuki@student.42tokyo.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 19:05:12 by hos               #+#    #+#             */
-/*   Updated: 2022/09/16 16:40:49 by hos              ###   ########.fr       */
+/*   Updated: 2022/09/17 19:08:25 by hos              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,23 @@ int	cmp_key(void *p1, void *p2)
 t_list	*init_list(t_environ *env)
 {
 	t_list	*list;
+	t_list	*list2;
 	t_var	*var;
 
 	if (!env)
 		return (NULL);
 	var = env->vars;
 	list = ft_lstnew(var);
+	if (!list)
+		ft_x_perror(EXIT_ERRMSG ": malloc");
 	if (var->next != NULL)
 		var = var->next;
-	while (var->next != NULL)
+	while (var != NULL)
 	{
-		ft_lstadd_back(&list, ft_lstnew(var));
+		list2 = ft_lstnew(var);
+		if (!list2)
+			ft_x_perror(EXIT_ERRMSG ": malloc");
+		ft_lstadd_back(&list, list2);
 		var = var->next;
 	}
 	return (list);
